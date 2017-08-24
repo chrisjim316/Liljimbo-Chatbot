d1b712d5fba13222ef2cd05ae39ceeeb5abf9d31
Chris Jim <chrisjim316@gmail.com>
2017-08-24 12:37:42 +0800

    update ask for help response with two more entities

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def webhook():
 						if entity == 'thanks':
 							response = "My pleasure. Always happy to help."
 						elif entity == 'help':
-							response = "Please indicate and type your situation. • burns • cuts and wound"
+							response = "Please indicate and type your situation. • burns • cuts and wound • black eyes • bruises"
 						elif entity == 'burns':
 							response = "1. Stop Burning Immediately 2." \
 							           " Remove Constrictive Clothing Immediately 3.Cover with sterile," \
