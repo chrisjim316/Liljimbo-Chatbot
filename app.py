d48d726eff62679101b8082e827dcdf70acdf1d7
Chris Jim <chrisjim316@gmail.com>
2017-09-03 00:53:46 +0800

    delete extraneous code

--- app.py
+++ app.py
@@ -90,16 +90,6 @@ def webhook():
 
 	return "ok", 200 # returns here, read bottom line
 
-    # hey, this code down here doesn't run because it returns up there ^^
-    # whats this doing? This is from the main branch :)
-	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-		if not request.args.get("hub.verify_token") == "hello":
-			return "Verification token mismatch", 403
-		return request.args["hub.challenge"], 200
-	return "Hello world", 200
-
-
-
 def exit():
 	# closes program
 	sys.exit(0)
